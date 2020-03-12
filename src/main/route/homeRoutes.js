551a1b949972960d80884528d01f29452e1dd835
dcassinotti <damiancassinotti.33@gmail.com>
2020-03-11 21:39:22 -0300

    Fix en countdown

--- src/main/route/homeRoutes.js
+++ src/main/route/homeRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/', (req, res) => {res.sendFile('index.html', { root: path.join(__dirname, '../webapp') })});
 
-router.get('/weddingDate', (req, res) => {res.json({"weddingDate": new Date(2020, 8, 26)})});
+router.get('/weddingDate', (req, res) => {res.json({"weddingDate": new Date(2020, 8, 26, 21)})});
 
 module.exports = router;
--- src/main/webapp/resources/js/controller/homeController.js
+++ src/main/webapp/resources/js/controller/homeController.js
@@ -2,10 +2,15 @@
     angular.module('hedwigApp').controller('homeController', ['$scope', 'data', '$interval', function ($scope, data, $interval) {
 
         $scope.data = data;
-        calculateCountdown()
+        $scope.data.weddingDate = new Date($scope.data.weddingDate);
+        calculateCountdown();
 
         function calculateCountdown() {
-            $scope.countdown = new Date(data.weddingDate).getTime() - (new Date()).getTime();
+            $scope.countdown = $scope.data.weddingDate - new Date();
+            $scope.countdownSeconds = Math.floor(($scope.countdown / 1000) % 60);
+            $scope.countdownMinutes = Math.floor(($scope.countdown / 60000) % 60);
+            $scope.countdownHours = Math.floor(($scope.countdown / 3600000) % 24);
+            $scope.countdownDays = Math.floor(($scope.countdown / (1000 * 60 * 60 * 24)));
         }
 
         $interval(calculateCountdown, 1000);
