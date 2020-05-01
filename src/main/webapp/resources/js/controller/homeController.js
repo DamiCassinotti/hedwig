8d1e3764ce13e0e2a6ffae8c99fd45fd7907d002
dcassinotti <damiancassinotti.33@gmail.com>
2020-05-01 20:38:30 -0300

    Added images and data

--- src/main/webapp/resources/js/controller/homeController.js
+++ src/main/webapp/resources/js/controller/homeController.js
@@ -3,6 +3,7 @@
 
         $scope.data = data;
         $scope.data.weddingDate = new Date($scope.data.weddingDate);
+        $scope.data.displayDatosCuenta = false;
         calculateCountdown();
 
         function calculateCountdown() {
@@ -11,6 +12,10 @@
             $scope.countdownMinutes = Math.floor(($scope.countdown / 60000) % 60);
             $scope.countdownHours = Math.floor(($scope.countdown / 3600000) % 24);
             $scope.countdownDays = Math.floor(($scope.countdown / (1000 * 60 * 60 * 24)));
+        }
+
+        $scope.toggleDatosCuenta = function() {
+            $scope.data.displayDatosCuenta = !$scope.data.displayDatosCuenta;
         }
 
         $interval(calculateCountdown, 1000);
