d94d2cd5c9eb33c496eec6f59d8a5bc2e0682ae7
DamiCassinotti <damiancassinotti@gmail.com>
2021-01-24 12:56:35 -0300

    Change wedding time

--- src/main/route/homeRoutes.js
+++ src/main/route/homeRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {res.sendFile('index.html', { root: path.join(__dirname, '../webapp') })});
 
-router.get('/weddingDate', (req, res) => {res.json({"weddingDate": new Date("2021-03-20T21:00:00.000-03:00")})});
+router.get('/weddingDate', (req, res) => {res.json({"weddingDate": new Date("2021-03-20T16:30:00.000-03:00")})});
 
 router.post('/confirmAssistance', (req, res) => {
 	confirmationController.confirmAssistance(req.body)
